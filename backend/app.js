b15503f810c90ff35ee34fd92c3ab6021f399827
mahiamali <mahiamali@gmail.com>
2025-06-21 11:21:57 +0530

    mahi_21_06_25_02

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const mongoose = require("mongoose");
 const app = express()
 const port = 3000
+const categoryRoutes = require("./routes/category")
 
+app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Server Running...')
 })
 
+// Mongo DB Connect
+async function connectDB() {
+  mongoose.connect("mongodb://localhost:27017/", {dbName:"e-comm-store-db"})  
+  // console.log("monogodb connected")
+}
+
+connectDB().catch((err)=>{
+  console.error(err)
+})
+
+// App Listen
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//----- API Start ------------------------------------------------
+app.use("/category",categoryRoutes)
---
+++ backend/routes/category.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const router = express.Router()
+const Category = require("./../db/category");
+const { addCategory, updateCategory, deleteCategory } = require("../handlers/category-handler");
+
+router.post("", async(req, res)=>{
+    let model = req.body;
+    let result = await addCategory(model);
+    res.send(result);
+})
+
+router.put("/:id", async(req, res)=>{
+    let model = req.body;
+    let id = req.params['id'];
+    await updateCategory(id, model);
+    res.send({message:"Updated!"}); 
+})
+
+router.delete("/:id", async(req, res)=>{
+    let id = req.params['id'];
+    await deleteCategory(id);
+    res.send({message:"Deleted!"}); 
+})
+
+module.exports = router
