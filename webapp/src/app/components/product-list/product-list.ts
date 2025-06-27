6a17280ede64b5ed835f0472d37f6b832136fbf0
mahiamali <mahiamali@gmail.com>
2025-06-27 12:48:57 +0530

    mahi_27_06_25_01

--- webapp/src/app/components/product-list/product-list.ts
+++ webapp/src/app/components/product-list/product-list.ts
@@ -5,10 +5,12 @@ import { ProductCard } from '../product-card/product-card';
 import { ActivatedRoute } from '@angular/router';
 import { Category } from '../../types/category';
 import { Brand } from '../../types/brand';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductCard],
+  imports: [ProductCard, FormsModule, NgClass],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
@@ -20,30 +22,31 @@ export class ProductList {
   sortBy: string = '';
   sortOrder: number = -1;
   page = 1;
-  pageSize = 60;
+  currentPage: number = 1;
+  pageSize = 3;
   products: Product[] = [];
-  route = inject(ActivatedRoute)
-  categories:Category[] =[]
-  brands:Brand[] =[]
+  route = inject(ActivatedRoute);
+  categories: Category[] = [];
+  brands: Brand[] = [];
 
   ngOnInit() {
     this.customerService.getCategories().subscribe((result) => {
       this.categories = result;
-    })
+    });
     this.customerService.getBrands().subscribe((result) => {
       this.brands = result;
-    })
-    this.route.queryParams.subscribe((x:any)=>{
+    });
+    this.route.queryParams.subscribe((x: any) => {
       console.log(x);
       this.searchTerm = x.search || '';
       this.categoryId = x.categoryid || '';
-      // this.brandId = x.brandid;
+      this.brandId = x.brandid || '';
 
       this.getProducts();
-    })
+    });
   }
 
-  getProducts(){
+  getProducts() {
     this.customerService
       .getProducts(
         this.searchTerm,
@@ -59,4 +62,29 @@ export class ProductList {
       });
   }
 
+  onSortChange(event: any) {
+    const value = +event.target.value; // Convert string to number
+    if (value === 0) {
+      this.sortBy = '';
+      this.sortOrder = -1;
+    } else {
+      this.sortBy = 'price';
+      this.sortOrder = value;
+    }
+
+    this.getProducts();
+  }
+
+  get totalPagesArray(): number[] {
+    return Array(this.pageSize)
+      .fill(0)
+      .map((_, i) => i + 1);
+  }
+
+  goToPage(page: number) {
+    if (page < 1 || page > this.pageSize) return;
+    
+    this.page = page;
+    this.getProducts(); // re-fetch products based on page
+  }
 }
